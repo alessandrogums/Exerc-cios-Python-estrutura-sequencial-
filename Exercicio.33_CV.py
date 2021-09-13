75ad80fbdb06b51214661f5833efb26215e9dc14
alessandrogums <90655009+alessandrogums@users.noreply.github.com>
2021-09-13 20:52:21 -0300

    Create Exercicio.33_CV.py

---
+++ Exercicio.33_CV.py
@@ -0,0 +1,18 @@
+#Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+
+n1=int(input(' digite o primeiro numero:'))
+n2=int(input(' digite o segundo numero:'))
+n3=int(input(' digite o terceiro numero:'))
+if n1>n2 and n1>n3:
+   print(f' o maior número é {n1}')
+elif n2>n3 and n2>n1:
+   print(f' o maior numero é {n2}')
+else:
+   print(f' o maior numero é {n3}')
+menor n
+if n1<n2 and n1<n3:
+   print(f' o menor número é {n1}')
+elif n2<n1 and n2<n3:
+  print(f' o menor número é {n2}')
+else:
+   print(f'o menor número é {n3}')
